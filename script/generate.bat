8463c2b682a92eb317590f4ea5a551ca8018ee88
null122 <1229608477@qq.com>
2023-06-04 01:54:36 +0800

    init

--- script/generate.bat
+++ script/generate.bat
@@ -10,7 +10,8 @@ for /r "%protoDir%" %%f in (*.proto) do (
     set "relativePath=!fileDir:%CD%\proto%=!"
 
 
-    echo Command: protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD%\ --http_python_out=%CD%\ !filePath!
+    echo Command: protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --pyi_out=%CD% --http_python_out=%CD%\ !filePath!
     protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD% --http_python_out=%CD% !filePath!
+    python -m grpc.tools.protoc --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --pyi_out=%CD% !filePath!
     echo.
 )
