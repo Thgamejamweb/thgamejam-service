ca1a6998ba07a38738df9fc1c5a1c7148d296704
null122 <1229608477@qq.com>
2023-06-03 22:25:59 +0800

    init

--- script/generate.bat
+++ script/generate.bat
@@ -8,17 +8,9 @@ for /r "%protoDir%" %%f in (*.proto) do (
     set "filePath=%%f"
     set "fileDir=%%~dpf"
     set "relativePath=!fileDir:%CD%\proto%=!"
-    set "apiPath=%CD%\api!relativePath!"
-    set "apiPath=!apiPath:~0,-1!"
-    echo proto path: !filePath!
-    echo api path: !apiPath!
 
-    if not exist "!apiPath!" (
-        echo Creating directory: !apiPath!
-        mkdir "!apiPath!"
-    )
 
-    echo Command: protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD%\api --http_python_out=%CD%\api !filePath!
-    protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD%\api --http_python_out=%CD%\api !filePath!
+    echo Command: protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD%\ --http_python_out=%CD%\ !filePath!
+    protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD% --http_python_out=%CD% !filePath!
     echo.
 )
