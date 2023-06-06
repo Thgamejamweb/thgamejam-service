6bf49f867c2d5823c90f08df31a011582d008266
null122 <1229608477@qq.com>
2023-06-06 22:15:02 +0800

    feat: services自动注入

--- app/main.py
+++ app/main.py
@@ -1,19 +1,15 @@
+import glob
+import importlib
 
 import uvicorn
 
-from app.services.user import UserServiceImpl
-from app.utils.router_register import register_fastapi_route, parse_request, parse_reply, app
-from thgamejam.api.user.user_pb2_http import register_user_http_server
+from app.utils.router_register import app
 
-
-
-
-# service_file = glob.glob("services/*.py")
+service_file = glob.glob('services/*.py')
 
 if __name__ == '__main__':
-    # for file_path in service_file:
-    #     module_name = file_path.replace(".py", "").replace("\\", ".")
-    #     print(module_name)
-    #     module = importlib.import_module(module_name)
-    register_user_http_server(register_fastapi_route, UserServiceImpl(), parse_request, parse_reply)
+    # services注入
+    for file in service_file:
+        module_name = file[:-3].replace('\\', '.')
+        module = importlib.import_module(module_name)
     uvicorn.run(app)
--- app/services/user.py
+++ app/services/user.py
@@ -1,6 +1,5 @@
-from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-
-from thgamejam.api.user.user_pb2_http import UserServicer
+from app.utils.router_register import register_fastapi_route, parse_request, parse_reply
+from thgamejam.api.user.user_pb2_http import UserServicer, register_user_http_server
 from thgamejam.api.user import user_pb2 as thgamejam_dot_api_dot_user_dot_user__pb2
 
 
@@ -11,3 +10,6 @@ class UserServiceImpl(UserServicer):
         reply = thgamejam_dot_api_dot_user_dot_user__pb2.TestReply()
         reply.test1 = request.test
         return reply
+
+
+register_user_http_server(register_fastapi_route, UserServiceImpl(), parse_request, parse_reply)
