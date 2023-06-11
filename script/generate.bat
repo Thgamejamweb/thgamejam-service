a08a349c7adfbac3d3faffe81fb3089cbb15d6dd
null122 <1229608477@qq.com>
2023-06-11 15:16:21 +0800

    fix: 调整api生成位置

--- script/generate.bat
+++ script/generate.bat
@@ -11,7 +11,7 @@ for /r "%protoDir%" %%f in (*.proto) do (
 
 
     echo Command: protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --pyi_out=%CD% --http_python_out=%CD%\ !filePath!
-    protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD%\app --http_python_out=%CD%\app !filePath!
-    python -m grpc.tools.protoc --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --pyi_out=%CD%\app !filePath!
+    protoc --proto_path=. --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --python_out=%CD% --http_python_out=%CD% !filePath!
+    python -m grpc.tools.protoc --proto_path=%CD%\proto --proto_path=%CD%\proto\third_party --pyi_out=%CD% !filePath!
     echo.
 )
