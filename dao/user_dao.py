db7554564336b976044cce9248dd39c6a988a1e4
null122 <1229608477@qq.com>
2023-06-15 22:58:54 +0800

    fix: 优化api异常抛出

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -22,36 +22,20 @@ def get_userinfo_by_id(userid: int, session: Session) -> UserEntity | None:
     return session.query(UserEntity).get(userid)
 
 
-def update_userinfo(user: UserEntity, session: Session) -> bool:
-    try:
-        session.merge(user)
-        session.commit()
-        return True
-    except:
-        session.rollback()
-        return False
+def update_userinfo(user: UserEntity, session: Session):
+    session.merge(user)
+    session.commit()
 
 
 def create_userinfo(user: UserEntity, session: Session) -> UserEntity:
-    try:
-        session.add(user)
-        session.commit()
-        return user
-    except Exception as e:
-        session.rollback()
-        print(f"Error occurred while creating user: {e}")
-        return None
+    session.add(user)
+    session.commit()
+    return user
 
 
 def verify_user_password(username: str, password: str, session: Session) -> UserEntity:
-    try:
-        user = session.query(UserEntity).filter(UserEntity.name == username, UserEntity.deleted == False).one()
+    user = session.query(UserEntity).filter(UserEntity.name == username, UserEntity.deleted == False).one()
 
-        psw = decrypt_data(password, user.private_key)
-        if psw == user.password:
-            return user
-        else:
-            raise HTTPException(status_code=403, detail="Password error")
-
-    except NoResultFound:
-        raise HTTPException(status_code=404, detail="UserInfo can not found or password is incorrect")
+    psw = decrypt_data(password, user.private_key)
+    if psw == user.password:
+        return user
--- services/user_service.py
+++ services/user_service.py
@@ -22,16 +22,16 @@ class UserServiceImpl(UserServicer):
             raise HTTPException(status_code=404, detail="User not exist")
 
         # 验证公私钥是否存在
-        if user.public_key is None:
-            key = RSA.generate(2048)
-            user.private_key = key.export_key().decode('utf-8')
-            user.public_key = key.publickey().export_key().decode('utf-8')
-
-            update_userinfo(user, session)
-
+        if user.public_key is not None:
             return GetUserPublicKeyReply(public_key=user.public_key)
-        else:
-            return GetUserPublicKeyReply(public_key=user.public_key)
+
+        key = RSA.generate(2048)
+        user.private_key = key.export_key().decode('utf-8')
+        user.public_key = key.publickey().export_key().decode('utf-8')
+
+        update_userinfo(user, session)
+
+        return GetUserPublicKeyReply(public_key=user.public_key)
 
     def Login(self, request: LoginRequest) -> LoginReply:
         session = database.get_db_session()
@@ -45,37 +45,32 @@ class UserServiceImpl(UserServicer):
         session = database.get_db_session()
 
         user_info = get_userinfo_by_username(request.username, session)
-        if user_info is None:
-            key = RSA.generate(2048)
-
-            user = UserEntity()
-            user.name = request.username
-            user.password = request.password
-            user.private_key = key.export_key().decode('utf-8')
-            user.public_key = key.publickey().export_key().decode('utf-8')
-            u = create_userinfo(user, session)
-
-            request_context.set(UserContext(userid=user.id))
-            return RegisterUserReply(id=u.id, username=u.name)
-        else:
+        if user_info is not None:
             raise HTTPException(status_code=404, detail="User has exist")
+
+        key = RSA.generate(2048)
+        user = UserEntity()
+        user.name = request.username
+        user.password = request.password
+        user.private_key = key.export_key().decode('utf-8')
+        user.public_key = key.publickey().export_key().decode('utf-8')
+        u = create_userinfo(user, session)
+
+        request_context.set(UserContext(userid=user.id))
+        return RegisterUserReply(id=u.id, username=u.name)
 
     def ChangePassword(self, request: ChangePasswordRequest) -> ChangePasswordReply:
         session = database.get_db_session()
-
         user = get_userinfo_by_id(request_context.get().userid, session)
         if user is None:
             raise HTTPException(status_code=404, detail="UserInfo not find")
-        else:
-            if user.password == request.old_password:
-                user.password = request.new_password
 
-                if update_userinfo(user, session):
-                    return ChangePasswordReply(id=user.id)
-                else:
-                    raise HTTPException(status_code=500, detail="error")
-            else:
-                raise HTTPException(status_code=401, detail="Password error")
+        if user.password != request.old_password:
+            raise HTTPException(status_code=401, detail="Password error")
+
+        user.password = request.new_password
+        update_userinfo(user, session)
+        return ChangePasswordReply(id=user.id)
 
 
 register_user_http_server(register_fastapi_route, UserServiceImpl(), parse_request, parse_reply)
