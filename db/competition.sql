8066d26c5aed496722cd55a606a71a428ded8b51
null122 <1229608477@qq.com>
2023-06-21 20:23:48 +0800

    feat: 增加competition的api，完善competition数据库字段

--- db/competition.sql
+++ db/competition.sql
@@ -5,6 +5,12 @@ CREATE TABLE competition
     staff_id        BIGINT(20)                 NOT NULL     COMMENT '主办方id',
     description     VARCHAR(100)               NULL         COMMENT '比赛简介描述',
     header_imageURL VARCHAR(255)               NULL         COMMENT '头图',
+    signup_start_date   DATETIME               NULL         COMMENT '报名开始日期',
+    signup_end_date     DATETIME               NULL         COMMENT '报名结束日期',
+    start_date          DATETIME               NULL         COMMENT '比赛开始日期',
+    end_date            DATETIME               NULL         COMMENT '比赛结束日期',
+    score_start_date    DATETIME               NULL         COMMENT '评分开始日期',
+    score_end_date      DATETIME               NULL         COMMENT '评分结束日期',
     ctime           DATETIME                   NULL         COMMENT '创建时间',
     mtime           DATETIME                   NULL         COMMENT '修改时间',
     deleted         BOOLEAN DEFAULT FALSE      NOT NULL     COMMENT '是否删除'
