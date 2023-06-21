d44a79e96e1013c0997a099489f4ad0cc2f1617f
Jack <2091813161@qq.com>
2023-06-21 18:32:37 +0800

    fix: 修复部分数据库字段

--- db/team.sql
+++ db/team.sql
@@ -1,7 +1,7 @@
 CREATE TABLE team
 (
     id              BIGINT(20) PRIMARY KEY AUTO_INCREMENT   COMMENT 'id',
-    name            VARCHAR(20)                NOT NULL     COMMENT '团队名称'
+    name            VARCHAR(20)                NOT NULL     COMMENT '团队名称',
     ctime           DATETIME                   NULL         COMMENT '创建时间',
     mtime           DATETIME                   NULL         COMMENT '修改时间',
     deleted         BOOLEAN DEFAULT FALSE      NOT NULL     COMMENT '是否删除'
--- db/works_info.sql
+++ db/works_info.sql
@@ -1,11 +1,10 @@
 CREATE TABLE works_info
 (
     id              BIGINT(20) PRIMARY KEY AUTO_INCREMENT   COMMENT 'id',
-    name            VARCHAR(64)                NOT NULL     COMMENT '作品名',
     team_id         BIGINT(20)                 NOT NULL     COMMENT '团队id',
     image_url_list  TEXT                       NOT NULL     COMMENT '轮播图url',
     content         TEXT                       NOT NULL     COMMENT '比赛介绍富文本',
-    works_url       TEXT                       NOT NULL     COMMENT '作品下载url',
+    file_id         BIGINT(20)                 NOT NULL     COMMENT '作品文件id',
     ctime           DATETIME                   NULL         COMMENT '创建时间',
     mtime           DATETIME                   NULL         COMMENT '修改时间',
     deleted         BOOLEAN DEFAULT FALSE      NOT NULL     COMMENT '是否删除'
